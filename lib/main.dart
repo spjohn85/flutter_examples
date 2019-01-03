d4662fcb5cd8d8ff96595ecd2e88dfc16580a9c7
John Williams <spjohn85@gmail.com>
2019-01-02 17:18:04 -0700

    added ui components

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'src/article.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
@@ -27,41 +25,288 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Article> _articles = articles;
+  int getColorHexFromStr(String colorStr) {
+    colorStr = "FF" + colorStr;
+    colorStr = colorStr.replaceAll("#", "");
+    int val = 0;
+    int len = colorStr.length;
+    for (int i = 0; i < len; i++) {
+      int hexDigit = colorStr.codeUnitAt(i);
+      if (hexDigit >= 48 && hexDigit <= 57) {
+        val += (hexDigit - 48) * (1 << (4 * (len - 1 - i)));
+      } else if (hexDigit >= 65 && hexDigit <= 70) {
+        // A..F
+        val += (hexDigit - 55) * (1 << (4 * (len - 1 - i)));
+      } else if (hexDigit >= 97 && hexDigit <= 102) {
+        // a..f
+        val += (hexDigit - 87) * (1 << (4 * (len - 1 - i)));
+      } else {
+        throw new FormatException("An error occurred when converting a color");
+      }
+    }
+    return val;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: _articles.map(_buildItem).toList(),
-      ),
+    return new Scaffold(
+        body: Column(
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  height: 150.0,
+                  width: double.infinity,
+                  color: Color(getColorHexFromStr('#FDD148')),
+                ),
+                Positioned(
+                  bottom: 50.0,
+                  right: 100.0,
+                  child: Container(
+                    height: 400.0,
+                    width: 400.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(200.0),
+                        color: Color(getColorHexFromStr('#FEE16D'))
+                            .withOpacity(0.4)),
+                  ),
+                ),
+                Positioned(
+                  bottom: 100.0,
+                  left: 150.0,
+                  child: Container(
+                    height: 300.0,
+                    width: 300.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(150.0),
+                        color: Color(getColorHexFromStr('#FEE16D'))
+                            .withOpacity(0.4)),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 25.0),
+                    Row(
+                      children: <Widget>[
+                        SizedBox(width: 15.0),
+                        Container(
+                          alignment: Alignment.topLeft,
+                          height: 50.0,
+                          width: 50.0,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25.0),
+                              border: Border.all(
+                                  color: Colors.white,
+                                  style: BorderStyle.solid,
+                                  width: 1.0),
+                              image: DecorationImage(
+                                  image: AssetImage('assets/chris.jpg'))),
+                        ),
+                        SizedBox(
+                            width: MediaQuery.of(context).size.width - 120.0),
+                        Container(
+                          alignment: Alignment.topRight,
+                          child: IconButton(
+                            icon: Icon(Icons.menu),
+                            onPressed: () {},
+                            color: Colors.white,
+                            iconSize: 30.0,
+                          ),
+                        ),
+                        SizedBox(height: 15.0)
+                      ],
+                    ),
+                    SizedBox(height: 10.0),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15.0),
+                      child: Text('Welcome ' + 'Johnny'),
+                    ),
+                    SizedBox(height: 25.0),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                      child: Material(
+                        elevation: 10.0,
+                        borderRadius: BorderRadius.circular(25.0),
+                        child: TextFormField(
+                            decoration: InputDecoration(
+                                border: InputBorder.none,
+                                prefixIcon: Icon(Icons.search,
+                                    color:
+                                        Color(getColorHexFromStr('#FEE16D'))),
+                                contentPadding:
+                                    EdgeInsets.only(left: 15.0, top: 15.0),
+                                hintText: 'Search for recipes',
+                                hintStyle: TextStyle(color: Colors.grey))),
+                      ),
+                    ),
+                    SizedBox(height: 10.0)
+                  ],
+                )
+              ],
+            ),
+            _recipeStack()
+          ],
+        ),
+      ],
+    ));
+  }
+
+  Widget _recipeStack() {
+    return Stack(
+      children: <Widget>[
+        Container(
+          //height: 500.0,
+          color: Color(getColorHexFromStr('#FDA147')),
+        ),
+        Column(
+          children: <Widget>[
+            SizedBox(height: 15.0),
+            Padding(
+              padding: EdgeInsets.only(left: 15.0),
+              child: Container(
+                padding: EdgeInsets.only(left: 10.0),
+                decoration: BoxDecoration(
+                    border: Border(
+                        left: BorderSide(
+                            color: Colors.orange,
+                            style: BorderStyle.solid,
+                            width: 3.0))),
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('POPULAR RECIPES',
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                fontFamily: 'Timesroman',
+                                fontWeight: FontWeight.bold))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 10.0),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 10.0, left: 10.0, right: 10.0),
+              height: 370.0,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                children: <Widget>[
+                  _recipeCard(),
+                  SizedBox(height: 10.0),
+                  _recipeCard(),
+                  SizedBox(height: 10.0),
+                  _recipeCard()
+                ],
+              ),
+            )
+          ],
+        )
+      ],
     );
   }
 
-  Widget _buildItem(Article e) {
-    return new Padding(
-      key: Key(e.text),
-      padding: const EdgeInsets.all(16.0),
-      child: ExpansionTile(
-        title: Text(e.text, style: new TextStyle(fontSize: 24.0)),
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+  Widget _recipeCard() {
+    return Padding(
+        padding: EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
+        child: Container(
+          height: 150.0,
+          width: double.infinity,
+          color: Colors.white,
+          child: Row(
             children: <Widget>[
-              Text("${e.commentsCount} comments"),
-              IconButton(
-                icon: Icon(Icons.launch),
-                color: Colors.green,
-                onPressed: () async {
-                  final url= "${e.domain}";
-                  if(await canLaunch(url)){
-                    launch(url);
-                  }
-                }
+              Container(
+                width: 140.0,
+                height: 150.0,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/food.jpg'),
+                        fit: BoxFit.contain)),
               ),
+              SizedBox(width: 4.0),
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text('Honey Cake',
+                          style: TextStyle(
+                              fontSize: 15.0, fontWeight: FontWeight.bold)),
+                      SizedBox(width: 45.0),
+                      Material(
+                        elevation: 2.0,
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Container(
+                          height: 30.0,
+                          width: 30.0,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15.0),
+                              color: Colors.white),
+                          child: Center(
+                            child: Icon(Icons.favorite_border, color: Colors.red),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+
+  Widget _foodCard() {
+    return Container(
+      height: 100.0,
+      width: 150.0,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0), color: Colors.white),
+      child: Row(
+        children: <Widget>[
+          Container(
+            height: 75.0,
+            width: 75.0,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(37.5),
+                image: DecorationImage(image: AssetImage('assets/food.jpg'))),
+          ),
+          SizedBox(width: 20.0),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Honey Cake', style: TextStyle(fontFamily: 'Quicksand')),
+              Text('Madras Samayal', style: TextStyle(fontFamily: 'Quicksand')),
+              SizedBox(height: 10.0),
+              Container(
+                height: 2.0,
+                width: 75.0,
+                color: Colors.orange,
+              ),
+              SizedBox(height: 10.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    height: 25.0,
+                    width: 25.0,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.5),
+                        image: DecorationImage(
+                            image: AssetImage('assets/chris.jpg'))),
+                  ),
+                  SizedBox(width: 10.0),
+                  Text('James Oliver',
+                      style: TextStyle(fontFamily: 'Quicksand'))
+                ],
+              )
             ],
           )
         ],
